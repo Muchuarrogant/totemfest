1f8eff4f49c864c407b27e22df163c0acd631ed8
Victor Matei <victormatei@hotmail.fr>
2016-02-16 17:22:59 +0100

    creating pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Totemfest::Application.routes.draw do
+  root "pages#home"
+  get "totem" => "pages#totem"
+  get "totemiv" => "pages#totemiv"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
