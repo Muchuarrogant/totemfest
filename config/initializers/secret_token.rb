1ccbdc4d7e1f444541b8968191f99acb3bc69d69
Victor Matei <victormatei@hotmail.fr>
2016-02-16 16:11:54 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Totemfest::Application.config.secret_key_base = '8a0567e4bed3d8c879c26e65b29aefc19a39066fe2732ab1dda62a32f66763448832088ad9471ef5be83eab4ef86a0491ec2fbe4cc1261d371251b1898d00273'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Totemfest::Application.config.session_store :cookie_store, key: '_Totemfest_session'
